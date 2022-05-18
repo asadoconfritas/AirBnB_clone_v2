7632743a5c0305ff279f609763d444af31f76f8a
root <guzchelsea24@gmail.com>
2022-05-18 18:14:06 +0000

    check 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -12,6 +12,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello HBNB!'
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host="0.0.0.0")
